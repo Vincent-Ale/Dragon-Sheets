8d47aa9f40a80945d5dea218c622f94cee03a0cc
Vincent-Ale <vincent.aleman@laposte.net>
2024-06-20 11:24:18 +0200

    Settings visuel OK

--- resources/views/components/toggle.blade.php
+++ resources/views/components/toggle.blade.php
@@ -5,8 +5,8 @@
 <label for="{{ $name }}" class="flex items-center cursor-pointer">
     <div class="relative">
         <input id="{{ $name }}" type="checkbox" class="sr-only" name="{{ $name }}" {{ $checked ? 'checked' : '' }}>
-        <div class="block bg-gray-600 w-14 h-8 rounded-full"></div>
-        <div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
+        <div class="block toggle-color w-14 h-8 rounded-full"></div>
+        <div class="dot absolute left-1 top-1 toggle-color2 w-6 h-6 rounded-full transition"></div>
     </div>
     <div class="ml-3 text-gray-700 font-medium">
         {{ $slot }}
@@ -15,11 +15,15 @@
 
 <style>
     input:checked + .block {
-        background-color: #4CAF50;
+        background-color: #2A3D80;
     }
 
     input:checked + .block + .dot {
         transform: translateX(100%);
+    }
+
+    input:checked + .block + .dot {
+        background-color: #3eaa9d;
     }
 
     .dot {
--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -9,12 +9,16 @@
         </h2>
     </x-slot>
 
+    <div class="bg-gravel w-72 h-16 ml-2.5 flex items-center justify-center rounded-lg mt-4">
+        <div class="bg-blue flex w-[17rem] h-12 rounded-lg text-center items-center justify-center pt-2 ">
+            <h1 class="font-volk-cyan text-4xl" >Paramètres</h1>
+        </div>
+    </div>
 
-    <div class="bg-white mx-auto">
-        <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-            <div class="mt-8 text-2xl">
-               Vos paramètres
-            </div>
+
+    <div class="mx-auto">
+        <div class="p-6 sm:px-20">
+        
 
             <form method="POST" action="{{ route('settings.update') }}">
                 @csrf
@@ -22,8 +26,8 @@
 
                 <div class="mt-6 text-gray-500">
                     <div>
-                        <strong>Thème de couleur :</strong>
-                        <select name="color_theme" id="color_theme" class="block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                        <strong class="font-trade-orange text-2xl">Thème de couleur</strong>
+                        <select name="color_theme" id="color_theme" class="font-volk-cyan block w-full mt-4 bg-blue-char rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                             <option value="blue" {{ ($settings['color_theme'] ?? '') == 'blue' ? 'selected' : '' }}>Blue</option>
                             <option value="green" {{ ($settings['color_theme'] ?? '') == 'green' ? 'selected' : '' }}>Green</option>
                             <option value="red" {{ ($settings['color_theme'] ?? '') == 'red' ? 'selected' : '' }}>Red</option>
@@ -31,20 +35,21 @@
                             <option value="purple" {{ ($settings['color_theme'] ?? '') == 'purple' ? 'selected' : '' }}>Purple</option>
                         </select>
                     </div>
+                    <p class="font-trade-orange text-2xl mt-16">Options</p>
                     <div class="mt-4">
-                        <x-toggle name="virtual_dices" :checked="$settings['virtual_dices'] ?? false">
-                            Dés virtuels
+                        <x-toggle class="" name="virtual_dices" :checked="$settings['virtual_dices'] ?? false">
+                            <span class="font-volk-cyan text-xl">Dés virtuels</span>
                         </x-toggle>
                     </div>
                     <div class="mt-4">
                         <x-toggle name="musical_theme" :checked="$settings['musical_theme'] ?? false">
-                            Thème musical
+                            <span class="font-volk-cyan text-xl">Thème musical</span>
                         </x-toggle>
                     </div>
                 </div>
 
                 <div class="mt-6">
-                    <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:bg-blue-600">
+                    <button type="submit" class="btn-modif mr-4 inline-flex items-center text-2xl px-4 py-2 border border-transparent rounded-full">
                         Enregistrer les paramètres
                     </button>
                 </div>
